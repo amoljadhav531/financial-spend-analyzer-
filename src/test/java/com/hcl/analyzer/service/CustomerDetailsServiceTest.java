18526eeba876914eaff366380bc8af731840ce98
Harsimar <simarkaur1295@gmail.com>
2019-07-25 16:47:47 +0530

    JUnit Fund Transfer

--- src/test/java/com/hcl/analyzer/service/CustomerDetailsServiceTest.java
+++ src/test/java/com/hcl/analyzer/service/CustomerDetailsServiceTest.java
@@ -1,26 +1,26 @@
-package com.hcl.analyzer.service;
-
-import org.junit.runner.RunWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.junit.MockitoJUnitRunner;
-
-import com.hcl.analyzer.dto.CustomerDetailsDto;
-import com.hcl.analyzer.entity.CustomerDetails;
-import com.hcl.analyzer.repository.CustomerDetailsRepository;
-
-@RunWith(MockitoJUnitRunner.class)
-public class CustomerDetailsServiceTest {
-	@InjectMocks
-	CustomerDetailsService customerDetailService;
-	
-	@Mock
-	CustomerDetailsRepository customerDetailsRepository;
-	
-	public void setCustomerDetails(CustomerDetailsDto customerDetailsDto)
-	{
-		CustomerDetails customerDetails=new CustomerDetails();
-		
-	}
-
-}
+//package com.hcl.analyzer.service;
+//
+//import org.junit.runner.RunWith;
+//import org.mockito.InjectMocks;
+//import org.mockito.Mock;
+//import org.mockito.junit.MockitoJUnitRunner;
+//
+//import com.hcl.analyzer.dto.CustomerDetailsDto;
+//import com.hcl.analyzer.entity.CustomerDetails;
+//import com.hcl.analyzer.repository.CustomerDetailsRepository;
+//
+//@RunWith(MockitoJUnitRunner.class)
+//public class CustomerDetailsServiceTest {
+//	@InjectMocks
+//	CustomerDetailsService customerDetailService;
+//	
+//	@Mock
+//	CustomerDetailsRepository customerDetailsRepository;
+//	
+//	public void setCustomerDetails(CustomerDetailsDto customerDetailsDto)
+//	{
+//		CustomerDetails customerDetails=new CustomerDetails();
+//		
+//	}
+//
+//}
