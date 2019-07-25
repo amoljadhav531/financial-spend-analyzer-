19c8d3e5117fecd4ed8b1a12b370d4e485325c5c
piyush <piyushr999@gmail.com>
2019-07-25 11:49:22 +0530

    Added git ignore file

---
+++ src/main/java/com/hcl/analyzer/entity/TransactionDetail.java
@@ -0,0 +1,33 @@
+package com.hcl.analyzer.entity;
+
+import java.time.LocalDate;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.Data;
+
+@Data
+@Entity
+public class TransactionDetail {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long transactionId;
+	
+	private String customerId;
+	
+	private LocalDate transactionDate;
+	
+	private String transactionType;
+	
+	private Double transactionAmount;
+	
+	private String transactionStatus;
+	
+	private String transactionOtp;
+	
+	private Double availableBalance;
+}
