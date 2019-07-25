2275de520f971c6ecb0b68a1b2f5fa797d9d0dc3
karanrathod123 <karanrathod2256@gmail.com>
2019-07-25 18:05:51 +0530

    commit logger

--- src/main/java/com/hcl/analyzer/controller/FundTranferController.java
+++ src/main/java/com/hcl/analyzer/controller/FundTranferController.java
@@ -1,5 +1,7 @@
 package com.hcl.analyzer.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -19,11 +21,13 @@ import lombok.extern.slf4j.Slf4j;
 @RestController
 public class FundTranferController {
 	
+	private static final Logger logger = LoggerFactory.getLogger(FundTranferController.class);
 	@Autowired
 	private FundTransferService fundTransferService;
 	 
 	@PostMapping("/transfer") 
 	public ResponseEntity<Object> fundTransfer(@RequestBody FundTransferDTO fundTransferDTO) {
+		logger.debug("entering into fundTransfer==========>>>>>>>>>>>>>>");
 		log.info("Creating New transactions for customer id" +fundTransferDTO.getCustomerId());
 		return new ResponseEntity<>(fundTransferService.fundTransfer(fundTransferDTO), HttpStatus.OK);
 	}
