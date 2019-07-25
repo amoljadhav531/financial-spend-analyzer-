55bf2ce26b92aab81b3326e88bf37dd713a6a4b3
karanrathod123 <karanrathod2256@gmail.com>
2019-07-25 16:55:37 +0530

    search transaction history api

--- src/main/java/com/hcl/analyzer/repository/TransactionDetailRepository.java
+++ src/main/java/com/hcl/analyzer/repository/TransactionDetailRepository.java
@@ -12,6 +12,9 @@ import com.hcl.analyzer.entity.TransactionDetail;
 @Repository
 public interface TransactionDetailRepository extends JpaRepository<TransactionDetail, Long> {
 
+	@Query(value = "SELECT * FROM transaction_detail  WHERE customer_id = :customer", nativeQuery = true)
+	List<TransactionDetail> findByCustomerDetails(@Param("customer") Long customer);
+	
 	@Query(value = "select * from transaction_detail where customer_id = :customerId AND MONTH(transaction_date) = :month AND YEAR(transaction_date) =:year",nativeQuery = true)
 	public List<TransactionDetail> findByMonthAndYear(@Param("customerId") Long customerId, @Param("month") String month, @Param("year") String year);
 }
--- src/test/java/com/hcl/analyzer/service/FundTransferServiceImpl.java
+++ src/test/java/com/hcl/analyzer/service/FundTransferServiceImpl.java
@@ -1,20 +1,16 @@
-package com.hcl.analyzer.service;
-
-import static org.junit.Assert.*;
-
-import org.junit.Before;
-import org.junit.Test;
-
-public class FundTransferServiceImpl {
-
-	@Before
-	public void setUp() throws Exception {
-	}
-
-	@Test
-	public void testFundTransfer() {
-		assertTrue(true);
-		//fail("Not yet implemented");
-	}
-
-}
+/*
+ * package com.hcl.analyzer.service;
+ * 
+ * import static org.junit.Assert.*;
+ * 
+ * import org.junit.Before; import org.junit.Test;
+ * 
+ * public class FundTransferServiceImpl {
+ * 
+ * @Before public void setUp() throws Exception { }
+ * 
+ * @Test public void testFundTransfer() { assertTrue(true);
+ * //fail("Not yet implemented"); }
+ * 
+ * }
+ */
