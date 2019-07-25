19ee4e429c69624126edf8d6c205bf4b39b7b829
Harsimar <simarkaur1295@gmail.com>
2019-07-25 16:26:30 +0530

    JUnit Fund Transfer

--- src/main/java/com/hcl/analyzer/controller/TransactionController.java
+++ src/main/java/com/hcl/analyzer/controller/TransactionController.java
@@ -5,6 +5,7 @@ import javax.validation.constraints.NotNull;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,6 +16,7 @@ import com.hcl.analyzer.service.TransactionHistoryService;
 
 @RestController
 @RequestMapping("/transaction")
+@CrossOrigin
 public class TransactionController {
 	
 	@Autowired
